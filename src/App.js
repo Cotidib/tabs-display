9e629ee237599800b4d01115a9737c0e05a408e4
Cotidib <constanza.dibueno@gmail.com>
2021-09-01 13:38:57 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React , {useState, useEffect} from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa'
+
+const url = 'https://course-api.com/react-tabs-project'; //provided by John Smilga
+
+function App() {
+  const [data, setData] = useState([]);
+  const [currentTab, setCurrentTab] = useState(0);
+  const [isLoading, setisLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const newData = await response.json();
+    setData(newData);
+    setisLoading(false);
+  }
+
+  useEffect( () => {
+    fetchData();
+  }, []);
+
+  if(isLoading){
+    return <section>
+      <h1>loading</h1>
+    </section>
+  }
+
+  else{
+    //destructuring after loading, so the data array isnt empty
+    const {company,dates,duties,title} = data[currentTab];
+
+    return (
+      <main>
+        <h1>Experience</h1>
+        <section className='container'> 
+          <article className='tabs-container'>
+            {
+              data.map((job,index) => {
+                return <button className={`tab-btn ${index === currentTab && 'tab-active'}`} key={job.id} onClick={() => setCurrentTab(index)}>
+                  {job.title}
+                </button> 
+              })
+            }
+          </article>
+          <article className='info-container'>
+            <h2>{title}</h2>
+            <div>{company}</div>
+            <p>{dates}</p>
+          
+            {
+              duties.map((duty, index) => {
+                return <div key={index}>
+                  <p><FaAngleDoubleRight/>{duty}</p>
+                </div>
+              })
+            }
+            
+          </article>
+        </section>
+      </main>
+    );
+
+  }
+
+  
+}
+
+export default App;
