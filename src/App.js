f8d6b5537f93c808e45ed3febda2e121e6dfa76d
Cotidib <constanza.dibueno@gmail.com>
2021-09-02 10:43:57 -0300

    night mode style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React , {useState, useEffect} from 'react';
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import { FaAngleDoubleRight, FaSpinner } from 'react-icons/fa'
 
 const url = 'https://course-api.com/react-tabs-project'; //provided by John Smilga
 
@@ -7,6 +7,7 @@ function App() {
   const [data, setData] = useState([]);
   const [currentTab, setCurrentTab] = useState(0);
   const [isLoading, setisLoading] = useState(true);
+  const [nightMode, setNightsMode] = useState(false);
 
   const fetchData = async () => {
     const response = await fetch(url);
@@ -19,9 +20,14 @@ function App() {
     fetchData();
   }, []);
 
+  const handleOnChange = () => {
+    setNightsMode(!nightMode);
+    console.log(nightMode);
+  };
+
   if(isLoading){
     return <section>
-      <h1>loading</h1>
+      <FaSpinner className="loading"/>
     </section>
   }
 
@@ -30,19 +36,24 @@ function App() {
     const {company,dates,duties,title} = data[currentTab];
 
     return (
-      <main>
-        <h1 className='main-title'>Experience</h1>
-        <section className='container'> 
+      <main className={nightMode? 'nm-page' : 'page'}>
+        <label className='switch'>
+          <input type='checkbox' onChange={handleOnChange}/>
+          <span className='slider'/>
+        </label>
+
+        <h1 className={nightMode? 'nm-main-title' : 'main-title'}>Experience</h1>
+        <section className={nightMode? 'nm-container' : 'container'}> 
           <article className='tabs-container'>
             {
               data.map((job,index) => {
-                return <button className={`tab-btn ${index === currentTab && 'tab-active'}`} key={job.id} onClick={() => setCurrentTab(index)}>
+                return <button className={nightMode? `nm-tab-btn ${index === currentTab && 'nm-tab-active'}` : `tab-btn ${index === currentTab && 'tab-active'}`} key={job.id} onClick={() => setCurrentTab(index)}>
                   {job.company}
                 </button> 
               })
             }
           </article>
-          <article className='info-container'>
+          <article className={nightMode? 'nm-info-container' : 'info-container'}>
             <h2>{title}</h2>
             <div className='company'>{company}</div>
             <p className='dates'>{dates}</p>
@@ -50,7 +61,7 @@ function App() {
             {
               duties.map((duty, index) => {
                 return <div key={index}>
-                  <p><FaAngleDoubleRight className='icon'/>{duty}</p>
+                  <p className={nightMode? 'nm-duty' : 'duty'}><FaAngleDoubleRight className={nightMode? 'nm-icon' : 'icon'}/>{duty}</p>
                 </div>
               })
             }
